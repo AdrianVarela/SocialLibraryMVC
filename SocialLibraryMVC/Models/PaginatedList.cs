847721add130bb7356b8732ccec33e1f7d6fb8ff
jacotor <coyotus.jacob@gmail.com>
2022-07-27 15:28:39 -0500

    Something

---
+++ SocialLibraryMVC/Models/PaginatedList.cs
@@ -0,0 +1,29 @@
+﻿namespace SocialLibraryMVC.Models
+{
+    public class PaginatedList<T>: List<T>
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+        public bool PreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool NextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+    }
+}
